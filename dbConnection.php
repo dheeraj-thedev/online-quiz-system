c7beaaa486904d1cf1b0ecc3ecee1b67f8dd2a76
Dheeraj Singh <bond.deejay@gmail.com>
2020-06-13 15:07:43 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,3 +1,3 @@
 <?php
-	$con= new mysqli('localhost','root','','quiz')or die("Could not connect to mysql".mysqli_error($con));
+	$con= new mysqli('database-1.ceufbi9js3ao.ap-south-1.rds.amazonaws.com','admin','tb123456','quiz')or die("Could not connect to mysql".mysqli_error($con));
 ?>
